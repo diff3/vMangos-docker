0fc87d3baa031c7a40a7192fc1ed84f341217c04
diff3 <magnus@entropy.nu>
2022-12-22 23:50:08 +0100

    cloning issues

--- compile/entry.sh
+++ compile/entry.sh
@@ -1,18 +1,32 @@
 #!/bin/sh
 
-if [ ! -f /opt/core/README.md && ];then
-   cd /opt
+path='/opt/etc'
+cd $path
+
+echo "Hello"
+
+if [ ! -d "$path/core" ]; then
+   echo "no core"
    git clone -b development https://github.com/vmangos/core
-   mkdir -p /opt/core/build
-else
-   cd /opt/core/build
+elif [ -d "$path/core" ] && [ ! -d "$path/core/.git" ]; then
+   echo "core but no .git"
+   rm -r $path/core
+   git clone -b development https://github.com/vmangos/core
+elif [ -d "$path/core" ] && [ -d "$path/core/.git" ]; then
+   echo "core and .git"
+   cd core
    git pull
-   make clean
 fi
 
-cd /opt/core/build
+if [ ! -d "/opt/etc/core/build" ]; then
+   echo "create build"
+   mkdir -p /opt/etc/core/build
+fi
+
+cd $path/core/build
 
 cmake .. -DCMAKE_INSTALL_PREFIX=${PREFX} -DSUPPORTED_CLIENT_BUILD=${BUILD} -DUSE_EXTRACTORS=${EXTRACTORS} 
 
+make clean
 make -j$CORES
 make install
