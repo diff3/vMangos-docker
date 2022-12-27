0c1e35d660ae69d3591a9918e6c6541c935585bf
Magnus Pettersson <magnus@entropy.nu>
2022-12-27 16:33:24 +0100

    updated mangosd with autoconf

---
+++ mangosd/entry.sh
@@ -0,0 +1,32 @@
+cp /opt/server/etc/mangosd.conf.dist /opt/server/etc/mangosd.conf
+
+sed -i -e '/DataDir =/ s/= .*/= \"\/opt\/server\/data\"/' /opt/server/etc/mangosd.conf
+sed -i -e '/LogsDir =/ s/= .*/= \"\/opt\/server\/logs\"/' /opt/server/etc/mangosd.conf
+sed -i -e '/HonorDir =/ s/= .*/= \"\/opt\/server\/honor\"/' /opt/server/etc/mangosd.conf
+sed -i -e '/LoginDatabase\.Info              =/ s/= .*/= \"mariadb\;3306\;mangos\;mangos\;realmd\"/' /opt/server/etc/mangosd.conf
+sed -i -e '/WorldDatabase\.Info              =/ s/= .*/= \"mariadb\;3306\;mangos\;mangos\;mangos\"/' /opt/server/etc/mangosd.conf
+sed -i -e '/CharacterDatabase\.Info          =/ s/= .*/= \"mariadb\;3306\;mangos\;mangos\;characters\"/' /opt/server/etc/mangosd.conf
+sed -i -e '/LogsDatabase\.Info               =/ s/= .*/= \"mariadb\;3306\;mangos\;mangos\;logs\"/' /opt/server/etc/mangosd.conf
+sed -i -e '/GameType =/ s/= .*/= 6/' /opt/server/etc/mangosd.conf
+sed -i -e '/RealmZone =/ s/= .*/= 8/' /opt/server/etc/mangosd.conf
+sed -i -e '/Motd =/ s/= .*/= \"Welcome to Winterspring server\"/' /opt/server/etc/mangosd.conf
+sed -i -e '/AllowTwoSide\.Accounts =/ s/= .*/= 1/' /opt/server/etc/mangosd.conf
+sed -i -e '/AllowTwoSide\.Interaction\.Chat =/ s/= .*/= 1/' /opt/server/etc/mangosd.conf
+sed -i -e '/AllowTwoSide\.Interaction\.Channel =/ s/= .*/= 1/' /opt/server/etc/mangosd.conf
+sed -i -e '/GM\.JoinOppositeFactionChannels =/ s/= .*/= 1/' /opt/server/etc/mangosd.conf
+sed -i -e '/AllowTwoSide\.Interaction\.Group =/ s/= .*/= 1/' /opt/server/etc/mangosd.conf
+sed -i -e '/AllowTwoSide\.Interaction\.Guild =/ s/= .*/= 1/' /opt/server/etc/mangosd.conf
+sed -i -e '/AllowTwoSide\.Interaction\.Trade =/ s/= .*/= 1/' /opt/server/etc/mangosd.conf
+sed -i -e '/AllowTwoSide\.Interaction\.Auction =/ s/= .*/= 1/' /opt/server/etc/mangosd.conf
+sed -i -e '/AllowTwoSide\.Interaction\.Mail =/ s/= .*/= 1/' /opt/server/etc/mangosd.conf
+sed -i -e '/AllowTwoSide\.WhoList =/ s/= .*/= 1/' /opt/server/etc/mangosd.conf
+sed -i -e '/AllowTwoSide\.AddFriend =/ s/= .*/= 1/' /opt/server/etc/mangosd.conf
+sed -i -e '/Ra\.Enable =/ s/= .*/= 1/' /opt/server/etc/mangosd.conf
+sed -i -e '/AHBot\.Enable =/ s/= .*/= 1/' /opt/server/etc/mangosd.conf
+sed -i -e '/AHBot\.itemcount =/ s/= .*/= 5000/' /opt/server/etc/mangosd.conf
+
+./mangosd -c /opt/server/etc/mangosd.conf
+
+
+
+
