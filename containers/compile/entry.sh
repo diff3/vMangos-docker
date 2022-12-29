ab7ce473d22566ea21c59cb607cd2b8d2219d3cf
Magnus Pettersson <magnus@entropy.nu>
2022-12-29 23:01:43 +0100

    fixed compile, with env och conf update

--- containers/compile/entry.sh
+++ containers/compile/entry.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
 
+escape() {
+  local tmp=`echo $1 | sed 's/[^a-zA-Z0-9\s:]/\\\&/g'`
+  echo "$tmp"
+}
 
 CONFIG_PATH=/opt/server/etc
 
@@ -45,40 +49,49 @@ cd /opt/etc/core/build
 cmake .. -DCMAKE_INSTALL_PREFIX=/opt/server -DSUPPORTED_CLIENT_BUILD=$BUILD -DUSE_EXTRACTORS=$EXTRACTORS
 
 make clean
-make -j$CORES
-make install
+make -j $(nproc) install
 
 if [ -f /etc/realmd.conf.dist ]; then
    cp /etc/realmd.conf.dist /opt/server/etc
 fi 
 
-if [ -f /etc/realmd.conf.dist ]; then
+if [ -f /opt/server/etc/realmd.conf.dist ]; then
+   cp /opt/server/etc/realmd.conf.dist /opt/server/etc/realmd.conf
+fi 
+
+if [ -f /etc/mangosd.conf.dist ]; then
    cp /etc/mangosd.conf.dist /opt/server/etc
 fi 
 
-sed -i -e '/DataDir =/ s/= .*/= \"\/opt\/server\/data\"/' $CONFIG_PATH/mangosd.conf
-sed -i -e '/LogsDir =/ s/= .*/= \"\/opt\/server\/logs\"/' $CONFIG_PATH/mangosd.conf
-sed -i -e '/HonorDir =/ s/= .*/= \"\/opt\/server\/honor\"/' $CONFIG_PATH/mangosd.conf
-sed -i -e '/Warden\.ModuleDir             =/ s/= .*/= \"\/opt\/server\/warden\_modules\"/' $CONFIG_PATH/mangosd.conf
+if [ -f /opt/server/etc/mangosd.conf.dist ]; then
+   cp /opt/server/etc/mangosd.conf.dist /opt/server/etc/mangosd.conf
+fi 
 
-sed -i -e '/LoginDatabase\.Info              =/ s/= .*/= \"mariadb\;3306\;mangos\;mangos\;realmd\"/' $CONFIG_PATH/mangosd.conf
-sed -i -e '/WorldDatabase\.Info              =/ s/= .*/= \"mariadb\;3306\;mangos\;mangos\;mangos\"/' $CONFIG_PATH/mangosd.conf
-sed -i -e '/CharacterDatabase\.Info          =/ s/= .*/= \"mariadb\;3306\;mangos\;mangos\;characters\"/' $CONFIG_PATH/mangosd.conf
-sed -i -e '/LogsDatabase\.Info               =/ s/= .*/= \"mariadb\;3306\;mangos\;mangos\;logs\"/' $CONFIG_PATH/mangosd.conf
+# mangosd.conf
+sed -i -e "/DataDir =/ s/= .*/= $(escape $DATA_DIR_PATH)/" $CONFIG_PATH/mangosd.conf
+sed -i -e "/LogsDir =/ s/= .*/= $(escape $LOGS_DIR_PATH)/" $CONFIG_PATH/mangosd.conf
+sed -i -e "/HonorDir =/ s/= .*/= $(escape $HONOR_DIR_PATH)/" $CONFIG_PATH/mangosd.conf
+sed -i -e "/Warden.ModuleDir             =/ s/= .*/= $(escape $WARDEN_DIR_PATH)/" $CONFIG_PATH/mangosd.conf
 
-sed -i -e '/GameType =/ s/= .*/= 6/' $CONFIG_PATH/mangosd.conf
-sed -i -e '/RealmZone =/ s/= .*/= 8/' $CONFIG_PATH/mangosd.conf
-sed -i -e '/Motd =/ s/= .*/= \"Welcome to Winterspring server\"/' $CONFIG_PATH/mangosd.conf
+sed -i -e "/LoginDatabase.Info              =/ s/= .*/= \"$(escape $DB_CONTAINER)\;3306\;$(escape $SERVER_DB_USER)\;$(escape $SERVER_DB_PWD)\;realmd\"/" $CONFIG_PATH/mangosd.conf
+sed -i -e "/WorldDatabase.Info              =/ s/= .*/= \"$(escape $DB_CONTAINER)\;3306\;$(escape $SERVER_DB_USER)\;$(escape $SERVER_DB_PWD)\;mangos\"/" $CONFIG_PATH/mangosd.conf
+sed -i -e "/CharacterDatabase.Info          =/ s/= .*/= \"$(escape $DB_CONTAINER)\;3306\;$(escape $SERVER_DB_USER)\;$(escape $SERVER_DB_PWD)\;characters\"/" $CONFIG_PATH/mangosd.conf
+sed -i -e "/LogsDatabase.Info               =/ s/= .*/= \"$(escape $DB_CONTAINER)\;3306\;$(escape $SERVER_DB_USER)\;$(escape $SERVER_DB_PWD)\;logs\"/" $CONFIG_PATH/mangosd.conf
 
-sed -i -e '/Ra\.Enable =/ s/= .*/= 1/' $CONFIG_PATH/mangosd.conf
-sed -i -e '/AHBot\.Enable =/ s/= .*/= 1/' $CONFIG_PATH/mangosd.conf
-sed -i -e '/AHBot\.itemcount =/ s/= .*/= 5000/' $CONFIG_PATH/mangosd.conf
+sed -i -e "/GameType =/ s/= .*/= $(escape $GAME_TYPE)/" $CONFIG_PATH/mangosd.conf
+sed -i -e "/RealmZone =/ s/= .*/= $(escape $REALM_ZONE)/" $CONFIG_PATH/mangosd.conf
+sed -i -e "/Motd =/ s/= .*/= $(escape $MOTD_MSG)/" $CONFIG_PATH/mangosd.conf
 
-sed -i -e '/SOAP\.Enabled =/ s/= .*/= 1/' $CONFIG_PATH/mangosd.conf
-sed -i -e '/SOAP\.IP =/ s/= .*/= 0\.0\.0\.0/' $CONFIG_PATH/mangosd.conf
+sed -i -e "/Ra.Enable =/ s/= .*/= $(escape $RA_ENABLE)/" $CONFIG_PATH/mangosd.conf
+sed -i -e "/AHBot.Enable  =/ s/= .*/= $(escape $AH_ENABLE)/" $CONFIG_PATH/mangosd.conf
+sed -i -e "/AHBot.itemcount =/ s/= .*/= $(escape $AH_ITEM_COUNT)/" $CONFIG_PATH/mangosd.conf
 
-sed -i -e '/LogsDir =/ s/= .*/= \"\/opt\/server\/logs\"/' $CONFIG_PATH/realmd.conf
-sed -i -e '/PatchesDir =/ s/= .*/= \"\/opt\/server\/patches\"/' $CONFIG_PATH/realmd.conf
-sed -i -e '/LoginDatabaseInfo =/ s/= .*/= \"mariadb\;3306\;mangos\;mangos\;realmd\"/' $CONFIG_PATH/realmd.conf
+sed -i -e "/SOAP.Enabled =/ s/= .*/= $(escape $SOAP_ENABLE)/" $CONFIG_PATH/mangosd.conf
+sed -i -e "/SOAP.IP =/ s/= .*/= $(escape $SOAP_IP)/" $CONFIG_PATH/mangosd.conf
+
+# realmd.conf
+sed -i -e "/LogsDir =/ s/= .*/= $(escape $LOGS_DIR_PATH)/" $CONFIG_PATH/realmd.conf
+sed -i -e "/PatchesDir =/ s/= .*/= $(escape $PATCH_DIR_PATH)/" $CONFIG_PATH/realmd.conf
+sed -i -e "/LoginDatabaseInfo =/ s/= .*/= \"$(escape $DB_CONTAINER)\;3306\;$(escape $SERVER_DB_USER)\;$(escape $SERVER_DB_PWD)\;realmd\"/" $CONFIG_PATH/realmd.conf
 
 
